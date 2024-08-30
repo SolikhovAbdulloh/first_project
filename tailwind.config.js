328028f5e0d5664b603384c8936b56264b2034d2
SolihovAbdulloh <abdullasolikh@mail.com>
2024-08-30 08:38:36 +0500

    mijoz

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./html/*.{html,js}"],
+  theme: {
+    colors: {
+      oq: "#ffff",
+      kok: "#1fb6ff",
+      purple: "#7e5bef",
+      pushti: "#ff49db",
+      olovrang: "#ff7849",
+      yashil: "#13ce66",
+      yellow: "#ffc82c",
+      "gray-dark": "#273444",
+      gray: "#8492a6",
+      "gray-light": "#d3dce6",
+    },
+    fontFamily: {
+      sans: ["Roboto"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
